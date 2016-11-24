2b33493ffd80580aa7d8696dfb74cb56e38013f3
DJ Enriquez <denrie.enriquezjr@gmail.com>
2016-11-24 01:32:58 -0800

    Add http_block + require domains key

--- src/consul-templates/app.conf.tmpl.go
+++ src/consul-templates/app.conf.tmpl.go
@@ -1,13 +1,16 @@
-{{$domain := (env "DOMAIN" | toLower)}}
+{{ if key (print "http_block_config" ) }}
+{{ key (print "http_block_config" ) }}
+{{ end }}
+
 {{range services}}upstream {{ .Name }}{
         {{ range service .Name }}server {{.Address}}:{{.Port}};
         {{ end }}
 }
 {{end}}
 
-{{range services}}server {
+{{ range services }}{{ if key (print .Name "/domains" ) }}server {
   listen 80;
-  server_name {{ .Name }}.{{ $domain }}{{if key (print .Name "/domains" )}} {{ key (print .Name "/domains" ) }}{{ end }};
+  server_name {{ key (print .Name "/domains" ) }};
   {{ if key (print .Name "/server_block_config" ) }}{{ key (print .Name "/server_block_config" ) }}{{ end }}
   location / {
     {{ if key (print .Name "/location_block_config" ) }}{{ key (print .Name "/location_block_config" ) }}{{ end }}
@@ -19,4 +22,4 @@
     proxy_pass  http://{{ .Name}}{{if key (print .Name "/proxy_uri" )}}{{key (print .Name "/proxy_uri" )}}{{end}};
   }
 }
-{{end}}
+{{ end }}{{ end }}
