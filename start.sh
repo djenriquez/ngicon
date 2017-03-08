e4a18e3efd230a89f4be20d7010f427c7fbc629e
djenriquez <denrie.enriquezjr@gmail.com>
2017-03-08 00:01:38 -0800

    Adding stream specific configuration

--- start.sh
+++ start.sh
@@ -3,7 +3,10 @@ export > /etc/envvars
 
 # Initialize files
 touch /etc/nginx/nginx.conf
+
+mkdir -p /etc/nginx/stream.d/
+touch /etc/nginx/stream.d/stream.conf
+
 touch /etc/nginx/conf.d/app.conf
-touch /etc/nginx/conf.d/stream.conf
 
 /usr/bin/runsvdir /etc/service
